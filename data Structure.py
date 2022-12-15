46c2b36331481b24a9171e47a9c496fba6ec915c
DEVELoper <syabblogger@gmail.com>
2022-12-15 22:04:19 +0500

    data structure added partially

---
+++ data Structure.py
@@ -0,0 +1,14 @@
+print("Data Structure in python")
+
+list = ["apple", "banana", "mango"]
+list.append("grapes")
+list.insert(3, "orange")
+list2 = list.copy()
+list.count("banana")
+list.sort()
+list.index("banana")
+list.remove("banana")
+
+list.clear()
+
+
